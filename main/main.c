1026fea1dd07c88502e6be6314bbf0cfddeb7d61
Wesley Jose Santos <binary_quantum@outlook.com>
2023-06-19 19:28:54 -0300

    feat: main file changes

--- main/main.c
+++ main/main.c
@@ -14,20 +14,6 @@
 
 #include "ciot_app.h"
 
-// ciot_msg_response_t resp = {
-//     .err_code = 0,
-//     .request = CIOT_MSG_REQUEST_GET_STATUS,
-//     .data status = {
-//             .interface = CIOT_MSG_IF_WIFI,
-//             .data.wifi = {
-//                     .tcp.started = true,
-//                     .tcp.connected = true,
-//                 },
-//             },
-//         },
-//     },
-// };
-
 void app_main(void)
 {
     ciot_app_config_t app = {
@@ -42,11 +28,6 @@ void app_main(void)
         }
     };
     ciot_app_start(&app);
-    // cJSON *json = cJSON_CreateObject();
-    // ciot_msg_response_to_json(json, &resp);
-    // char *json_str = cJSON_Print(json);
-    // printf(json_str);
-    // free(json_str);
 }
 
 #if defined(WIN32) || defined(__linux__)
