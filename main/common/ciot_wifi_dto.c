dc15db7ba760cd1c6a50a5e38204c2264087613d
Wesley Jose Santos <binary_quantum@outlook.com>
2023-06-19 20:32:56 -0300

    feat: create timeout parameter

--- main/common/ciot_wifi_dto.c
+++ main/common/ciot_wifi_dto.c
@@ -16,6 +16,7 @@ ciot_err_t ciot_wifi_config_from_json(CJSON_PARAMETERS(ciot_wifi_config_t))
     CJSON_CHECK_PARAMETERS();
     CJSON_GET_CHAR_ARRAY(ssid);
     CJSON_GET_CHAR_ARRAY(password);
+    CJSON_GET_NUMBER(timeout);
     CJSON_GET_OBJ_FROM_ROOT(tcp, ciot_tcp_config_from_json);
     return CIOT_ERR_OK;
 }
@@ -25,6 +26,7 @@ ciot_err_t ciot_wifi_config_to_json(CJSON_PARAMETERS(ciot_wifi_config_t))
     CJSON_CHECK_PARAMETERS();
     CJSON_ADD_CHAR_ARRAY(ssid);
     CJSON_ADD_CHAR_ARRAY(password);
+    CJSON_ADD_NUMBER(timeout);
     CJSON_ADD_OBJ_TO_ROOT(tcp, ciot_tcp_config_to_json);
     return CIOT_ERR_OK;
 }
