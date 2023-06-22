eaf4cd6e917bbc2b839bf271614006f17d36c2f2
Wesley Jose Santos <binary_quantum@outlook.com>
2023-06-21 21:30:19 -0300

    feat: move wifi type parameter to wifi_config_t struct

--- main/include/ciot_wifi.h
+++ main/include/ciot_wifi.h
@@ -24,6 +24,7 @@ typedef enum ciot_wifi_type
 
 typedef struct ciot_wifi_config
 {
+    ciot_wifi_type_t type;
     char ssid[32];
     char password[64];
     int timeout;
@@ -40,7 +41,7 @@ typedef struct ciot_wifi_status
     ciot_tcp_status_t tcp;
 } ciot_wifi_status_t;
 
-ciot_err_t ciot_wifi_set_config(ciot_wifi_type_t type, ciot_wifi_config_t *conf);
+ciot_err_t ciot_wifi_set_config(ciot_wifi_config_t *conf);
 ciot_err_t ciot_wifi_get_config(ciot_wifi_type_t type, ciot_wifi_config_t *config);
 ciot_err_t ciot_wifi_get_status(ciot_wifi_type_t type, ciot_wifi_status_t *status);
 ciot_err_t ciot_wifi_get_info(ciot_wifi_type_t type, ciot_wifi_info_t *info);
