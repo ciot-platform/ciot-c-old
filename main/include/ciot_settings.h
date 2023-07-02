9cf3aa6741fd14f7030727f55cf5f0acf3699677
Wesley Jose Santos <binary_quantum@outlook.com>
2023-07-01 21:20:00 -0300

    feat: add mqtt dto to settings

--- main/include/ciot_settings.h
+++ main/include/ciot_settings.h
@@ -11,12 +11,14 @@
 
 #include "ciot_ntp.h"
 #include "ciot_wifi.h"
+#include "ciot_mqtt.h"
 
 typedef struct ciot_settings
 {
     ciot_wifi_config_t wifi_ap;
     ciot_wifi_config_t wifi_sta;
     ciot_ntp_config_t ntp;
+    ciot_mqtt_config_t mqtt;
 } ciot_settings_t;
 
 ciot_err_t ciot_settings_save(void);
