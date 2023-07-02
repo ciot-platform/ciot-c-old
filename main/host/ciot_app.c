c70f31342d2fc6de01822f637c73fbe0e9760786
Wesley Jose Santos <binary_quantum@outlook.com>
2023-07-01 22:20:01 -0300

    feat: move mongoose to ciot_app host implementation

--- main/host/ciot_app.c
+++ main/host/ciot_app.c
@@ -10,6 +10,7 @@
  */
 
 #include "ciot_app.h"
+#include "mongoose.h"
 
 #ifdef _WIN32
 #include "windows.h"
@@ -17,80 +18,43 @@
 #include <pthread.h>
 #endif
 
+#ifdef _WIN32
+#define ciot_thread_t HANDLE
+#else
+#define ciot_thread_t pthread_t
+#endif
+
 typedef struct ciot_app_data
 {
     ciot_msg_t msg;
     bool msg_received;
     bool msg_processed;
-#ifdef _WIN32
-    HANDLE thread;
-#else
-    pthread_t thread;
-#endif
+    ciot_thread_t thread;
 } ciot_app_data_t;
 
 static ciot_app_data_t this;
+struct mg_mgr mgr;
 
-#ifdef _WIN32
-
-static DWORD WINAPI ciot_app_task(LPVOID args)
-{
-    printf("CIOT Application Start\n");
-
-    for(;;)
-    {
-        if(this.msg_received)
-        {
-            ciot_err_t err = ciot_app_msg_handle(&this.msg);
-            this.msg_received = false;
-            this.msg_processed = true;
-            printf("Message %d processed with err code %x\n", this.msg.type, err);
-        }
-    }
-}
+static void* ciot_app_task(void* args);
+static ciot_err_t ciot_app_create_thread();
 
 ciot_err_t ciot_app_start(ciot_app_config_t *conf)
 {
-    this.thread = CreateThread(NULL, 0, &ciot_app_task, NULL, 0, NULL);
-    if (this.thread == NULL)
-    {
-        return CIOT_ERR_FAIL;
-    }
-    return ciot_app_init(conf);
-}
-
-#else
-
-static void* ciot_app_task(void* args)
-{
-    printf("CIOT Application Start\n");
-
-    while (1)
-    {
-        if (this.msg_received)
-        {
-            ciot_err_t err = ciot_app_msg_handle(&this.msg);
-            this.msg_received = false;
-            this.msg_processed = true;
-            printf("Message %d processed with err code %x\n", this.msg.type, err);
-        }
-    }
-
-    return NULL;
-}
-
-ciot_err_t ciot_app_start(ciot_app_config_t* conf)
-{
-    int result = pthread_create(&this.thread, NULL, ciot_app_task, NULL);
-
-    if (result != 0)
+    if (ciot_app_create_thread())
     {
         return CIOT_ERR_FAIL;
     }
 
-    return ciot_app_init(conf);
+    mg_mgr_init(&mgr);
+    ciot_app_init(conf);
+
+    while(true){
+        mg_mgr_poll(&mgr, 1000);
+    }
+    mg_mgr_free(&mgr);
+
+    return CIOT_ERR_OK;
 }
-#endif
 
 ciot_err_t ciot_app_send_msg(ciot_msg_t *msg)
 {
@@ -118,5 +82,39 @@ ciot_err_t ciot_app_wait_process(uint32_t timeout)
         }
     }
     this.msg_processed = false;
+    return CIOT_ERR_OK;
+}
+
+static void* ciot_app_task(void* args)
+{
+    printf("CIOT Application Start\n");
+
+    for(;;)
+    {
+        if(this.msg_received)
+        {
+            ciot_err_t err = ciot_app_msg_handle(&this.msg);
+            this.msg_received = false;
+            this.msg_processed = true;
+            printf("Message %d processed with err code %x\n", this.msg.type, err);
+        }
+    }
+}
+
+static ciot_err_t ciot_app_create_thread()
+{
+#ifdef _WIN32
+    this.thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)&ciot_app_task, NULL, 0, NULL);
+    if (this.thread == NULL)
+    {
+        return CIOT_ERR_FAIL;
+    }
+#else
+    int result = pthread_create(&this.thread, NULL, ciot_app_task, NULL);
+    if (result != 0)
+    {
+        return CIOT_ERR_FAIL;
+    }
+#endif
     return CIOT_ERR_OK;
 }
--- main/main.c
+++ main/main.c
@@ -13,18 +13,15 @@
 #include <stdio.h>
 
 #include "ciot_app.h"
-#include "ciot_config.h"
 
 void app_main(void)
 {
     ciot_app_config_t app = {
-#if CIOT_CONFIG_FEATURE_WIFI
         .wifi = {
             .mode = CIOT_CONFIG_WIFI_AP_MODE,
             .ssid = CIOT_CONFIG_WIFI_AP_SSID,
             .password = CIOT_CONFIG_WIFI_AP_PASSWORD,
         },
-#endif
         .http_server = {
             .port = CIOT_CONFIG_HTTP_SERVER_PORT,
             .endpoint = CIOT_CONFIG_HTTP_SERVER_ENDPOINT
@@ -36,18 +33,10 @@ void app_main(void)
 
 #if defined(_WIN32) || defined(__linux__)
 
-#include "mongoose.h"
-
-struct mg_mgr mgr;
-
 int	main(int argc, char **argv)
 {
-    mg_mgr_init(&mgr);
     app_main();
-    while(true){
-        mg_mgr_poll(&mgr, 1000);
-    }
-    mg_mgr_free(&mgr);
+    return 0;
 }
 
 #endif  //_WIN32
