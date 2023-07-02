4aae5260e9efd000eaab40c7aa00700b98a6ba64
Wesley Jose Santos <binary_quantum@outlook.com>
2023-07-01 21:19:22 -0300

    feat: implement mqtt settings

--- main/common/ciot_settings.c
+++ main/common/ciot_settings.c
@@ -20,12 +20,12 @@ ciot_err_t ciot_settings_save(void)
 
 #if CIOT_CONFIG_FEATURE_WIFI
     err = ciot_wifi_get_config(CIOT_WIFI_IF_AP, &settings.wifi_ap);
-    if (err == CIOT_ERR_OK)
+    if (err != CIOT_ERR_OK)
     {
         CIOT_ERROR_PRINT(ciot_storage_save_data(&settings.wifi_ap, sizeof(settings.wifi_ap), CIOT_CONFIG_WIFI_AP_FILENAME));
     }
     err = ciot_wifi_get_config(CIOT_WIFI_IF_STA, &settings.wifi_sta);
-    if (err == CIOT_ERR_OK)
+    if (err != CIOT_ERR_OK)
     {
         CIOT_ERROR_PRINT(ciot_storage_save_data(&settings.wifi_sta, sizeof(settings.wifi_sta), CIOT_CONFIG_WIFI_STA_FILENAME));
     }
@@ -33,7 +33,15 @@ ciot_err_t ciot_settings_save(void)
 
 #if CIOT_CONFIG_FEATURE_NTP
     err = ciot_ntp_get_config(&settings.ntp);
-    if (err == CIOT_ERR_OK)
+    if (err != CIOT_ERR_OK)
+    {
+        CIOT_ERROR_PRINT(ciot_storage_save_data(&settings.ntp, sizeof(settings.ntp), CIOT_CONFIG_NTP_FILENAME));
+    }
+#endif
+
+#if CIOT_CONFIG_FEATURE_MQTT
+    err = ciot_mqtt_get_config(&settings.mqtt);
+    if (err != CIOT_ERR_OK)
     {
         CIOT_ERROR_PRINT(ciot_storage_save_data(&settings.ntp, sizeof(settings.ntp), CIOT_CONFIG_NTP_FILENAME));
     }
@@ -55,6 +63,11 @@ ciot_err_t ciot_settings_load(ciot_settings_t *settings)
 
 #if CIOT_CONFIG_FEATURE_NTP
     err = ciot_storage_load_data(&settings->ntp, sizeof(settings->ntp), CIOT_CONFIG_NTP_FILENAME);
+    CIOT_ERROR_PRINT(err);
+#endif
+
+#if CIOT_CONFIG_FEATURE_MQTT
+    err = ciot_storage_load_data(&settings->mqtt, sizeof(settings->mqtt), CIOT_CONFIG_MQTT_FILENAME);
     CIOT_ERROR_PRINT(err);
 #endif
 
@@ -83,6 +96,15 @@ ciot_err_t ciot_settings_clear(void)
 
 #if CIOT_CONFIG_FEATURE_NTP
     err = ciot_storage_remove_data(CIOT_CONFIG_NTP_FILENAME);
+    if(err != CIOT_ERR_OK)
+    {
+        ret = err;
+        CIOT_ERROR_PRINT(ret);
+    }
+#endif
+
+#if CIOT_CONFIG_FEATURE_MQTT
+    err = ciot_storage_remove_data(CIOT_CONFIG_MQTT_FILENAME);
     if(err != CIOT_ERR_OK)
     {
         ret = err;
