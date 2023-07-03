fbf71c14370fa7957b8557f998db5ec4c0e2b5f5
Wesley Jose Santos <binary_quantum@outlook.com>
2023-07-03 19:51:22 -0300

    feat: return feedback to ciot_app

--- main/host/ciot_mqtt.c
+++ main/host/ciot_mqtt.c
@@ -15,11 +15,13 @@
 #include "ciot_app.h"
 #include "ciot_data_io.h"
 
+#define CIOT_MQTT_TIMEOUT 5000
+
 extern struct mg_mgr mgr;
 
 typedef struct ciot_mqtt
 {
-    struct mg_connection *s_conn;
+    void *handle;
     ciot_mqtt_config_data_t config;
     ciot_mqtt_info_t info;
     ciot_mqtt_status_t status;
@@ -32,6 +34,7 @@ static ciot_mqtt_t this;
 static void ciot_mqtt_event_handler(struct mg_connection *c, int ev, void *ev_data, void *fn_data);
 static ciot_err_t ciot_mqtt_set_config_connection(ciot_mqtt_config_connection_t *config);
 static ciot_err_t ciot_mqtt_set_config_topics(ciot_mqtt_config_topics_t *config);
+static void ciot_mqtt_wait_connection(int timeout);
 
 ciot_err_t ciot_mqtt_set_config(ciot_mqtt_config_t *conf)
 {
@@ -75,7 +78,7 @@ ciot_err_t ciot_mqtt_publish(const char *topic, const char *data, int len, int q
         .message = mg_str(data),
         .qos = qos,
         .retain = retain};
-    mg_mqtt_pub(this.s_conn, &opts);
+    mg_mqtt_pub(this.handle, &opts);
     return CIOT_ERR_OK;
 }
 
@@ -84,7 +87,7 @@ ciot_err_t ciot_mqtt_subscribe(const char *topic, int qos)
     struct mg_mqtt_opts opts = {
         .topic = mg_str(topic),
         .qos = qos};
-    mg_mqtt_sub(this.s_conn, &opts);
+    mg_mqtt_sub(this.handle, &opts);
     return CIOT_ERR_OK;
 }
 
@@ -105,16 +108,9 @@ static void ciot_mqtt_event_handler(struct mg_connection *c, int ev, void *ev_da
     ciot_err_t err = CIOT_ERR_OK;
     switch (ev)
     {
-    case MG_EV_OPEN:
-        printf("MG_EV_OPEN\n");
-        break;
     case MG_EV_ERROR:
         printf("MG_EV_ERROR:%d:%s\n", c->id, (char *)ev_data);
         this.status.state = CIOT_MQTT_STATE_ERROR;
-        break;
-    case MG_EV_CONNECT:
-        printf("MG_EV_CONNECT\n");
-        this.status.state = CIOT_MQTT_STATE_CONNECTING;
         break;
     case MG_EV_MQTT_OPEN:
         printf("MG_EV_MQTT_OPEN\n");
@@ -176,7 +172,7 @@ static void ciot_mqtt_event_handler(struct mg_connection *c, int ev, void *ev_da
     case MG_EV_CLOSE:
         printf("MG_EV_CLOSE\n");
         this.status.state = CIOT_MQTT_STATE_DISCONNECTED;
-        this.s_conn = NULL;
+        this.handle = NULL;
         break;
     default:
         break;
@@ -193,13 +189,28 @@ static ciot_err_t ciot_mqtt_set_config_connection(ciot_mqtt_config_connection_t 
         .version = 4,
         .user = mg_str(this.config.connection.username),
         .pass = mg_str(this.config.connection.password)};
-    if (this.s_conn == NULL)
-        this.s_conn = mg_mqtt_connect(&mgr, this.config.connection.host, &opts, ciot_mqtt_event_handler, NULL);
-    return CIOT_ERR_OK;
+    this.status.state = CIOT_MQTT_STATE_CONNECTING;
+    if (this.handle == NULL)
+        this.handle = mg_mqtt_connect(&mgr, this.config.connection.host, &opts, ciot_mqtt_event_handler, NULL);
+    ciot_mqtt_wait_connection(CIOT_MQTT_TIMEOUT);
+    return this.status.state == CIOT_MQTT_STATE_CONNECTED ? CIOT_ERR_OK : CIOT_ERR_FAIL;
 }
 
 static ciot_err_t ciot_mqtt_set_config_topics(ciot_mqtt_config_topics_t *conf)
 {
     memcpy(&this.config.topics, conf, sizeof(this.config.topics));
     return ciot_mqtt_subscribe(this.config.topics.message, this.config.topics.qos);
+}
+
+static void ciot_mqtt_wait_connection(int timeout)
+{
+    time_t start_time = time(0);
+    while (this.status.state == CIOT_MQTT_STATE_CONNECTING)
+    {
+        time_t current_time = time(0);
+        if((start_time + timeout) < current_time)
+        {
+            return;
+        }
+    }
 }
